93d0f89f36cb09f0107feac722fb224bd7872956
MeTi-DeV <mahdi_zarei78@yahoo.com>
2021-12-19 12:11:17 +0330

    add project and split app.component

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  newServerName = '';
+  newServerContent = '';
+  constructor() { }
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+  ngOnInit(): void {
+  }
+
+}
